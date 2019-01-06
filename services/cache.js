94609b3de70dd4d97d1db2b71a4601f97d2f70d9
avistheripper <bboyxns@mail.ru>
2019-01-06 14:57:11 +0300

    remark

--- services/cache.js
+++ services/cache.js
@@ -36,7 +36,6 @@ mongoose.Query.prototype.exec = async function () {
     }
 
     const result = await exec.apply(this, arguments);
-
     await client.set(key, JSON.stringify(result), 'EX', 10);
 
   return result;
