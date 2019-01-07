a8db385af24b68d522cbb2600e8c5d61636ca135
avistheripper <bboyxns@mail.ru>
2019-01-07 17:38:10 +0300

    Puppeteer test suit

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,3 +1,12 @@
+const puppet = require('puppeteer');
+
 test('Header component', () => {
     expect(2*2).toEqual(4);
+});
+
+test('Browser test', async () => {
+    const browser = await puppet.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
 })
