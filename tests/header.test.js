c396cdcc1dd34c746f2a46bdaca5f4378507c680
avistheripper <bboyxns@mail.ru>
2019-01-08 15:45:39 +0300

    Setting cache from the outside

--- tests/header.test.js
+++ tests/header.test.js
@@ -4,10 +4,13 @@ test('Header component', () => {
     expect(2*2).toEqual(4);
 });
 
-test('Browser test', async () => {
+test('Initial app test', async () => {
     const browser = await puppet.launch({
         headless: false
     });
     const page = await browser.newPage();
     await page.goto('localhost:3000');
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
 })
