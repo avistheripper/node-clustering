560a5c4e550f436024e5b4ade7e79f61af2df889
avistheripper <bboyxns@mail.ru>
2019-01-23 21:37:56 +0300

    Fake session implent

--- tests/header.test.js
+++ tests/header.test.js
@@ -24,3 +24,26 @@ test('Initializing the OAth flow', async () => {
     const url = await page.url();
     expect(url).toMatch(/accounts\.google\.com/)
 });
+
+test('Session setting and auth flow', async () => {
+    const uid = '5c26235de754b932bee1a7ed';
+
+    const buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: uid
+        }
+    };
+    const sessionString = 
+        buffer.from(JSON.stringify(sessionObject)).toString('base64');
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
+
+    
+})
